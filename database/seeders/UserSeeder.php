5fdfd3b0124c51ada2a05e6479cf690ee1c40fd0
walter <walter123asg@gmail.com>
2021-01-06 14:28:17 -0600

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Walter Alfonso Soriano',
+            'email' => 'walter123asg@gmail.com',
+            'password' => bcrypt('123')
+        ]);
+        
+        User::factory(99)->create(); 
+    }
+}
